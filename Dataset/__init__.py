ddfcd0c81b55fe50020dabf91570cd42eccd2493
Andrea Camilloni <s1090325@studenti.univpm.it>
2022-10-04 09:32:31 +0200

    fix threshold sentiment

--- Dataset/__init__.py
+++ Dataset/__init__.py
@@ -60,12 +60,12 @@ def get_dataset(path=os.path.join(sys.path[0], "Dataset\\dataset v.1.2")):
     reviews_df = pd.read_csv(path+"\\reviews_df.csv")
     users_df = pd.read_csv(path + "\\users.csv")
     wines_df = pd.read_csv(path + "\\wine_df.csv")
-    full_df = pd.read_csv(path + "\\full_dataset.csv")
+    full_df = pd.read_csv(path + "\\full_datasetV1_1.csv")
     return reviews_df,wines_df,users_df,full_df
 
 def create_label(rating):
     if rating > 4:
         return 1
-    elif rating == 4:
+    elif rating < 4:
         return -1
     return 0
