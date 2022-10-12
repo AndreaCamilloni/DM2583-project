20e0523505dcbc27c9e370a8334c72bcb0e957da
Andrea Camilloni <s1090325@studenti.univpm.it>
2022-10-12 16:07:03 +0200

    scraping script

--- WebScraping/vivino_scraper.py
+++ WebScraping/vivino_scraper.py
@@ -11,7 +11,7 @@ def get_user_data(username):
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
     }
 
-    api_url = "https://www.vivino.com/api/users/{username}"  # <-- increased the number of reviews to 9999
+    api_url = "https://www.vivino.com/api/users/{username}"
 
     data = requests.get(
         api_url.format(username=username), headers=headers
@@ -19,13 +19,33 @@ def get_user_data(username):
 
     return data
 
+def scrape_users(users_reviews_count):
+    print("Scraping users...")
+    user_data = []
+    tmp = 0
+    for u in users_reviews_count.index:
+        user_json = get_user_data(u)
+        user_data.append([
+            user_json['user']['seo_name'],
+            user_json['user']['bio'],
+            user_json['user']['address']['country']['seo_name']
+        ])
+        if tmp == 500:
+            time.sleep(5)
+            tmp = 0
+
+        tmp += 1
+
+    users = pd.DataFrame(user_data, columns=["User", "Bio", "Country"])
+    return users
+
 
 def get_wine_data(wine_id, year, page):
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
     }
 
-    api_url = "https://www.vivino.com/api/wines/{id}/reviews?per_page=50&year={year}&page={page}"  # <-- increased the number of reviews to 9999
+    api_url = "https://www.vivino.com/api/wines/{id}/reviews?per_page=50&year={year}&page={page}"
 
     data = requests.get(
         api_url.format(id=wine_id, year=year, page=page), headers=headers
@@ -35,6 +55,7 @@ def get_wine_data(wine_id, year, page):
 
 
 def vivino_scraper(p):
+    print("Scraping page {}".format(p))
     r = requests.get(
         "https://www.vivino.com/api/explore/explore",
         params={
@@ -82,9 +103,6 @@ def vivino_scraper(p):
     for _, row in dataframe.iterrows():
         page = 1
         while True:
-            # print(
-            #    f'Getting info about wine {row["Wine ID"]}-{row["Year"]} Page {page}'
-            # )
 
             d = get_wine_data(row["Wine ID"], row["Year"], page)
             # print(d)
--- scraping_script.py
+++ scraping_script.py
@@ -1,26 +1,45 @@
 # Run only if you want to scrape new data.
 # Consider that the scraper takes about 5 minutes to scrape each page
 # You can find al the craped data in the dataset folder.
+import os
+import sys
+
 import pandas as pd
 import time
 
-from WebScraping.vivino_scraper import vivino_scraper
+from WebScraping.vivino_scraper import vivino_scraper, scrape_users
+
+PAGES_TO_SCRAPE = 1
 
 p = 1 # page number
 wine_df = pd.DataFrame()
 reviews_df = pd.DataFrame()
-while p<2:
+while p<=PAGES_TO_SCRAPE:
     reviews, wines  = vivino_scraper(p)
     p += 1
     time.sleep(2)
-    print("page: ", p)
     if len(wines) != 0:
       wine_df = pd.concat([wine_df,wines])
       reviews_df = pd.concat([reviews_df,reviews])
 
 wine_df.reset_index()
 reviews_df.reset_index()
+full_df = pd.merge(reviews_df,wine_df,on=["Year", "Wine ID", "Wine"])
+
 
 users_reviews_count = reviews_df.pivot_table(columns=['User'], aggfunc='size')
 users_reviews_count = users_reviews_count.sort_values(ascending=False)
-print(users_reviews_count.index)
+
+users_df = scrape_users(users_reviews_count)
+
+print(users_df)
+print(wine_df)
+print(reviews_df)
+print(full_df)
+
+# Save dataframes to files
+print("Saving dataframes into Dataset\\dataset v.1.2")
+wine_df.to_csv(os.path.join(sys.path[0], "Dataset\\dataset v.1.2") + "\\wine_df.csv", index=False)
+reviews_df.to_csv(os.path.join(sys.path[0], "Dataset\\dataset v.1.2") + "\\reviews_df.csv", index=False)
+full_df.to_csv(os.path.join(sys.path[0], "Dataset\\dataset v.1.2") + "\\full_dataset.csv", index=False)
+users_df.to_csv(os.path.join(sys.path[0], "Dataset\\dataset v.1.2") + "\\users.csv", index=False)
